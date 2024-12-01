60263203ff5e9d940ff68f4409e2fbc9a3992529
nadannnd <138690271+nadannnd@users.noreply.github.com>
2024-12-01 20:11:45 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RAG App'
+copyright = '2025, Nada'
+author = 'Nada'
 
 release = '0.1'
 version = '0.1.0'
